8dea64111edda4045ad1286af6627d2162223ddc
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-01-19 17:05:53 +0200

    - moved components to separate components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
 
 const App = () => {
     return (
@@ -7,29 +9,6 @@ const App = () => {
             <Technologies />
         </div>
     );
-}
-
-const Technologies = () =>{
-    return (
-        <div>
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
-        </div>
-    )
-}
-
-const Header = () =>{
-    return (
-        <div>
-            <a href="#s">Home</a>
-            <a href="#s">News Feed</a>
-            <a href="#s">Messages</a>
-        </div>
-    )
 }
 
 export default App;
