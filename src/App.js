f8c7078a01fe0898629754dd2f8b91fee11cd382
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-01-30 01:40:12 +0200

    - 11 - added html markup

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import Header from "./components/Header";
 const App = () => {
     return (
         <div className="app-wrapper">
-           <Header />
+            <Header/>
             <nav className="nav">
                 <div>Profile</div>
                 <div>Messages</div>
                 <div>News</div>
                 <div>Music</div>
-                <div>other</div>
+                <div>Settings</div>
             </nav>
-            <div className="content">Main content</div>
+            <div className="content">
+                <div>
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT-_mIfmXfxZydIr5sSyiVu6p_LMiG6L8ZDw&usqp=CAU"/>
+                </div>
+                <div>
+                    ava+desc
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        new post
+                    </div>
+                    <div>
+                        <div>post 1</div>
+                        <div>post 2</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
