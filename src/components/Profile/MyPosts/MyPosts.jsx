49009005eaa87a4306399b43954ad9e7af72f453
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-01-31 14:39:29 +0200

    - 16 - add props to post components

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,13 +11,8 @@ const MyPosts = () => {
                     <button>Add post</button>
                 </div>
                 <div className={s.post}>
-                    <Post />
-                    <Post />
-                    <Post />
-                    <Post />
-                    <Post />
-                    <Post />
-
+                    <Post message="Hi, how are you"/>
+                    <Post message="It's my first post"/>
 
                 </div>
             </div>
